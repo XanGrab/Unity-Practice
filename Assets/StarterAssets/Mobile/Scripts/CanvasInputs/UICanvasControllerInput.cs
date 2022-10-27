c1d7008b0974d20a5aea38ff59c749d5d399f53e
XanGrab <sunbird.grabowski@gmail.com>
2022-10-27 12:43:51 -0500

    * began packaging my character controller 📦

--- Assets/StarterAssets/Mobile/Scripts/CanvasInputs/UICanvasControllerInput.cs
+++ Assets/StarterAssets/Mobile/Scripts/CanvasInputs/UICanvasControllerInput.cs
@@ -1,32 +1,30 @@
 using UnityEngine;
 
-namespace StarterAssets
-{
-    public class UICanvasControllerInput : MonoBehaviour
-    {
+namespace StarterAssets {
+    public class UICanvasControllerInput : MonoBehaviour {
 
-        [Header("Output")]
-        public CharacterInputs starterAssetsInputs;
+        // [Header("Output")]
+        // public CharacterInputs starterAssetsInputs;
 
-        public void VirtualMoveInput(Vector2 virtualMoveDirection)
-        {
-            starterAssetsInputs.MoveInput(virtualMoveDirection);
-        }
+        // public void VirtualMoveInput(Vector2 virtualMoveDirection)
+        // {
+        //     starterAssetsInputs.MoveInput(virtualMoveDirection);
+        // }
 
-        public void VirtualLookInput(Vector2 virtualLookDirection)
-        {
-            starterAssetsInputs.LookInput(virtualLookDirection);
-        }
+        // public void VirtualLookInput(Vector2 virtualLookDirection)
+        // {
+        //     starterAssetsInputs.LookInput(virtualLookDirection);
+        // }
 
-        public void VirtualJumpInput(bool virtualJumpState)
-        {
-            starterAssetsInputs.JumpInput(virtualJumpState);
-        }
+        // public void VirtualJumpInput(bool virtualJumpState)
+        // {
+        //     starterAssetsInputs.JumpInput(virtualJumpState);
+        // }
 
-        public void VirtualSprintInput(bool virtualSprintState)
-        {
-            starterAssetsInputs.SprintInput(virtualSprintState);
-        }
+        // public void VirtualSprintInput(bool virtualSprintState)
+        // {
+        //     starterAssetsInputs.SprintInput(virtualSprintState);
+        // }
         
     }
 
